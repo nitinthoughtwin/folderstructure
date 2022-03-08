2233aac0ad5f146d9eeaa1790d69e95ac116062e
nitinthoughtwin <nitin.thoughtwin@gmail.com>
2022-03-08 18:22:23 +0530

    Test branch

--- src/Routes/PublicRoutes.js
+++ src/Routes/PublicRoutes.js
@@ -3,4 +3,4 @@ import { Navigate, Route } from 'react-router-dom'
 export const PublicRoutes = ({Component , isUser}) => {
   const auth = isUser;
   return auth ?  <Navigate to="/" /> :<Component />
-}
+} 
